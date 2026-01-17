5fb2fa1ea216f6517ca42141c73e16881df6180d
Ivgeni Segal <ivgeni.segal@gmail.com>
2026-01-17 13:46:16 -0800

    Initial empty project

---
+++ bin/native_pty.dart
@@ -0,0 +1,5 @@
+import 'package:native_pty/native_pty.dart' as native_pty;
+
+void main(List<String> arguments) {
+  print('Hello world: ${native_pty.calculate()}!');
+}
---
+++ test/native_pty_test.dart
@@ -0,0 +1,8 @@
+import 'package:native_pty/native_pty.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
